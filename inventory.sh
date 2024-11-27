20058fc25f7b405c6f2ed3ddf4172f74b4785f04
tungtel <52432821+tungtel@users.noreply.github.com>
2024-11-27 11:16:34 +0100

    Update inventory.sh

--- inventory.sh
+++ inventory.sh
@@ -5,16 +5,17 @@ filelist="inventory_list.txt"
 
 # Output file to store the combined extracted output
 output_file="inventory_output.txt"
+
 > inventory_output.txt
 sed -i 's/ *$//' inventory_list.txt
+
 # Loop through each filename in the filelist
-while IFS= read -r inventory_list; do
+while IFS= read -r filelist; do
   # Execute the sed command and append the output to the output file
-  config_file="/var/rancid/routers/configs/$inventory_list"
+  config_file="/var/rancid/routers/configs/$filelist"
   sed -n '/show chassis hardware models/,/show chassis routing-engine/p' "$config_file" >> "$output_file"
 
   # Add a separator between outputs from different files (optional)
   echo "--------------------------------------------------------------------------------" >> "$output_file"
 done < "$filelist"
-
 echo "Combined output saved to: $output_file"
